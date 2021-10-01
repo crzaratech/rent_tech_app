b57dea303c557adc77b01de2c48897b7c488b0f4
Josef Vassell <Josefvassell@outlook.com>
2021-09-30 20:58:36 -0500

    forogt screen and the routes for the login and forgot screen

--- lib/InitialScreens/forgotpassword_screen.dart
+++ lib/InitialScreens/forgotpassword_screen.dart
@@ -1,28 +1,18 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:rent_tech/InitialScreens/signup_screen.dart';
-import 'package:rent_tech/InitialScreens/forgotpassword_screen.dart';
-import 'package:rent_tech/homescreen/home_screen.dart';
-
-
-class LoginScreen extends StatefulWidget {
-
+import 'package:rent_tech/InitialScreens/login_screen.dart';
+class Forgotscreen extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  _ForgotscreenState createState() => _ForgotscreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  bool isRememberMe = false;
-  Widget buildEmail() {
-
+class _ForgotscreenState extends State<Forgotscreen> {
+  Widget builduserEmail() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Text(
-          'Email',
+          'Please type in your email',
           style: TextStyle(
               color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
         ),
@@ -53,12 +43,12 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget buildPassword() {
+  Widget builduserPassword() {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Text(
-          'Password',
+          'Type in new password',
           style: TextStyle(
               color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
         ),
@@ -89,61 +79,54 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget buildForgotPassBtn() {
-    return Container(
-      alignment: Alignment.centerRight,
-      child: FlatButton(
-        onPressed: () {
-          Navigator.push(context, new MaterialPageRoute(builder: (context) => new Forgotscreen()));
-        },
-        padding: EdgeInsets.only(right: 0),
-        child: Text(
-          'Forgot password',
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+  Widget buildrestPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Retype in your new password',
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
         ),
-      ),
+        SizedBox(height: 10),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
+              ]),
+          height: 60,
+          child: TextField(
+              obscureText: true,
+              style: TextStyle(color: Colors.black87),
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 14),
+                  prefixIcon: Icon(Icons.lock, color: Color(0xff00bfff)),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                    color: Colors.black38,
+                  ))),
+        )
+      ],
     );
   }
-
-  Widget buildRememberMe() {
-    return Container(
-        height: 20,
-        child: Row(
-          children: <Widget>[
-            Theme(
-              data: ThemeData(unselectedWidgetColor: Colors.white),
-              child: Checkbox(
-                value: isRememberMe,
-                checkColor: Colors.blue,
-                activeColor: Colors.white,
-                onChanged: (value) {
-                  setState(() {
-                    isRememberMe = value!;
-                  });
-                },
-              ),
-            ),
-            Text(
-              'Remember me',
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ));
-  }
-
-  Widget buildLoginBtn() {
+ 
+  Widget buildloginBtn() {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 25),
       width: double.infinity,
       child: RaisedButton(
         elevation: 5,
-        onPressed: () {
-          Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => HomeScreen()));
-
+        onPressed: ()
+        {
+          Navigator.push(context, new MaterialPageRoute(builder: (context) => new LoginScreen()));
         },
-        padding: EdgeInsets.all(20),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        padding: EdgeInsets.all(25),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
         color: Colors.white,
         child: Text(
           'Login',
@@ -156,38 +139,13 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  Widget buildSignUpBtn() {
-    return GestureDetector(
-      onTap: () {
-         Navigator.push(context, new MaterialPageRoute(builder: (context) => new SignupScreen()));
-      },
-      child: RichText(
-        text: TextSpan(children: [
-          TextSpan(
-              text: 'Don\'t have an Accout?',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500)),
-          TextSpan(
-              text: ' Sign up',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold))
-        ]),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: GestureDetector(
-          child: Stack(
-            children: <Widget>[
+        body: AnnotatedRegion<SystemUiOverlayStyle>(
+            value: SystemUiOverlayStyle.light,
+            child: GestureDetector(
+                child: Stack(children: <Widget>[
               Container(
                 height: double.infinity,
                 width: double.infinity,
@@ -210,28 +168,23 @@ class _LoginScreenState extends State<LoginScreen> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Text(
-                        'Sign In',
+                        'Forgot Password',
                         style: TextStyle(
                             color: Colors.white,
                             fontSize: 36,
                             fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(height: 50),
-                      buildEmail(),
                       SizedBox(height: 20),
-                      buildPassword(),
-                      buildForgotPassBtn(),
-                      buildRememberMe(),
-                      buildLoginBtn(),
-                      buildSignUpBtn(),
+                      builduserEmail(),
+                      SizedBox(height: 20),
+                      builduserPassword(),
+                      SizedBox(height: 20),
+                      buildrestPassword(),
+                      buildloginBtn(),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ]))));
   }
 }
