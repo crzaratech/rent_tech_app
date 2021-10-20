f58745a49c78e9a7018ff41d7fa907b8373553bc
zaratech <54729832+crzaratech@users.noreply.github.com>
2021-10-20 17:05:26 -0500

    created widgets and functions with Leo, to pull images from the firebase database

--- lib/displaya_all_products/allproducts.dart
+++ lib/displaya_all_products/allproducts.dart
@@ -1,10 +1,15 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:rent_tech/authenticate/fire_auth.dart';
+import 'package:rent_tech/models/myuser.dart';
 
 class AllProducts extends StatefulWidget {
   @override
@@ -35,6 +40,7 @@ class _AllProductsState extends State<AllProducts> {
       maxWidth: 1080,
 
     );
+    //error on iphone
     _cropImage(pickedFile!.path);
     Navigator.pop(context);
   }
@@ -109,7 +115,50 @@ class _AllProductsState extends State<AllProducts> {
     );
   }
 
+  void _upload_images() async{
+    if(imageFile == null){
+      Fluttertoast.showToast(msg: "Please select an Image");
+      return;
+    }
+    try{
+      final ref = FirebaseStorage.instance.ref().child('userImages').child(DateTime.now().toString()+'jpg');
+      await ref.putFile(imageFile!);
+      imageUrl = await ref.getDownloadURL();
+      FirebaseFirestore.instance.collection('Products').doc(DateTime.now().toString()).set({
+        'id': AuthService().getCurrentUser(),
+        'email': AuthService().getCurrentUserEmail(),
+        'Image': imageUrl,
+        'createdAt': DateTime.now(),
+      });
+      //Navigator.canPop(context) ? Navigator.pop(context) : null;
+      imageFile = null;
+    }
+    catch(e){
+      Fluttertoast.showToast(msg: e.toString());
+    }
+  }
 
+  Widget gridViewWidget(String img){
+    return GridView.count(
+        primary: false,
+        padding: EdgeInsets.all(5),
+        crossAxisCount: 1,
+        crossAxisSpacing: 1,
+      children: [
+        Container(
+          padding: EdgeInsets.all(10),
+          child: GestureDetector(
+            onTap: (){},
+            child: Center(
+              child: Image.network(img, fit: BoxFit.fill,),
+            ),
+          ),
+          color: Colors.white,
+        )
+      ],
+
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -125,10 +174,12 @@ class _AllProductsState extends State<AllProducts> {
 
           actions: <Widget>[
             FlatButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('logout'),
+              icon: Icon(Icons.add_a_photo_outlined),
+              label: Text('add imgs'),
               onPressed: () async {
-                await FirebaseAuth.instance.signOut();
+                if(imageFile != null){
+                  _upload_images();
+                }
               },
             ),
           ],
@@ -140,12 +191,48 @@ class _AllProductsState extends State<AllProducts> {
           },
           child: const Icon(Icons.camera_enhance),
         ),
-        body: Center(
-          child: Center(
-            child: Text("This is the product screen?"),
-          ),
-        ),
+        
+        body:
+              StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection('Products').orderBy("createdAt",descending: true).snapshots(),
+                  builder: (context, snapshot){
+                  if(snapshot.connectionState == ConnectionState.waiting){
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  else if(snapshot.connectionState == ConnectionState.active){
+                    if(snapshot.data!.docs.isNotEmpty){
+                      return GridView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3
+                          ),
+                          itemBuilder: (BuildContext context, int index){
+                            return gridViewWidget(
+                                snapshot.data!.docs[index]['Image'],
+
+                            );
+                    });
+                    }
+                  }else{
+                    return Center(
+                      child: Text('There is no Data to get'),
+                    );
+                  }
+                  return Center(
+                    child: Text('Something went wrong'),
+                  );
+                  }
+
+
+              ),
+
+
+
+
+
       ),
     );
   }
 }
+
+
