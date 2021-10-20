4218eef07eec4e03012b56939361d633005d2cc3
johnlugo <johnathan.lugo01@utrgv.edu>
2021-10-19 21:10:54 -0500

    Added product categories to homescreen

---
+++ lib/productScreens/laptopsForRent.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class laptopsForRent extends StatefulWidget {
+  const laptopsForRent({Key? key}) : super(key: key);
+
+  @override
+  _laptopsForRentState createState() => _laptopsForRentState();
+}
+
+class _laptopsForRentState extends State<laptopsForRent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue);
+  }
+}
