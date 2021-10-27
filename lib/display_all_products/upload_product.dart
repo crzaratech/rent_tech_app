7f8a7865c4d80a08be1b7d242f2499bd338a0123
Leonardo Ruiz <lruiz0333@gmail.com>
2021-10-27 01:07:16 -0500

    add product_type, condition_value to the form

--- lib/display_all_products/upload_product.dart
+++ lib/display_all_products/upload_product.dart
@@ -14,7 +14,8 @@ class uploadProduct extends StatefulWidget {
   @override
   _uploadProduct createState() => _uploadProduct();
 }
-enum pTypes{laptops, desktops}
+
+enum pTypes { laptops, desktops }
 
 class _uploadProduct extends State<uploadProduct> {
   //firebase
@@ -26,72 +27,68 @@ class _uploadProduct extends State<uploadProduct> {
   //product data
   String? productCondition;
   bool isAvailable = true;
-  late TextEditingController productName = TextEditingController(text:'');
-  late TextEditingController price = TextEditingController(text:'');
-  pTypes? _productTypes ;
+  late TextEditingController productName = TextEditingController(text: '');
+  late TextEditingController price = TextEditingController(text: '');
+  late TextEditingController zipCode = TextEditingController(text: '');
+  pTypes? _productTypes;
   String? pId;
-
-
+  String? pTypesValue;
+  String? conditionValue;
 
 //radio button list
 
-  void _getFromGallery() async{
+  void _getFromGallery() async {
     XFile? pickedFile = await ImagePicker().pickImage(
       source: ImageSource.gallery,
       maxHeight: 1080,
       maxWidth: 1080,
-
     );
     _cropImage(pickedFile!.path);
     //Navigator.pop(context);
   }
 
-  void _getFromCamera() async{
+  void _getFromCamera() async {
     XFile? pickedFile = await ImagePicker().pickImage(
       source: ImageSource.camera,
       maxHeight: 1080,
       maxWidth: 1080,
-
     );
     //error on iphone
     _cropImage(pickedFile!.path);
     //Navigator.pop(context);
   }
 
-  void _cropImage(filePath) async{
+  void _cropImage(filePath) async {
     File? croppedImage = await ImageCropper.cropImage(
       sourcePath: filePath,
       maxHeight: 1080,
       maxWidth: 1080,
     );
-    if(croppedImage != null){
-      setState((){
+    if (croppedImage != null) {
+      setState(() {
         imageFile = croppedImage;
       });
     }
   }
 
-  void _showImageDialog(){
+  void _showImageDialog() {
     showDialog(
         context: context,
-        builder: (context){
+        builder: (context) {
           return AlertDialog(
             title: Text('Please choose an option'),
             content: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 InkWell(
-                  onTap: (){
+                  onTap: () {
                     _getFromGallery();
                   },
                   child: Row(
                     children: const [
                       Padding(
                         padding: EdgeInsets.all(4.0),
-                        child: Icon(
-                            Icons.photo,
-                            color: Colors.lightBlue
-                        ),
+                        child: Icon(Icons.photo, color: Colors.lightBlue),
                       ),
                       Text(
                         'Gallery',
@@ -101,17 +98,14 @@ class _uploadProduct extends State<uploadProduct> {
                   ),
                 ),
                 InkWell(
-                  onTap: (){
+                  onTap: () {
                     _getFromCamera();
                   },
                   child: Row(
                     children: const [
                       Padding(
                         padding: EdgeInsets.all(4.0),
-                        child: Icon(
-                            Icons.camera,
-                            color: Colors.lightBlue
-                        ),
+                        child: Icon(Icons.camera, color: Colors.lightBlue),
                       ),
                       Text(
                         'Camera',
@@ -123,42 +117,39 @@ class _uploadProduct extends State<uploadProduct> {
               ],
             ),
           );
-
-        }
-
-    );
-
+        });
   }
 
-  void _upload_images() async{
-    if(imageFile == null){
+  void _upload_images() async {
+    if (imageFile == null) {
       Fluttertoast.showToast(msg: "Please select an Image");
       return;
     }
-    try{
-      final ref = FirebaseStorage.instance.ref().child('userImages').child(DateTime.now().toString()+'jpg');
+    try {
+      final ref = FirebaseStorage.instance
+          .ref()
+          .child('userImages')
+          .child(DateTime.now().toString() + 'jpg');
       await ref.putFile(imageFile!);
       imageUrl = await ref.getDownloadURL();
       FirebaseFirestore.instance.collection('Products').doc().set({
-
         'createdAt': DateTime.now(),
         'fromUserID': AuthService().getCurrentUser(),
         'email': AuthService().getCurrentUserEmail(),
         'Image': imageUrl,
-       // 'product_Id' :
-        'Product_Name' : productName.text,
-        'Product_Price' : price.text,
+        // 'product_Id' :
+        'Product_Name': productName.text,
+        'Product_Price': price.text,
         'is_available': isAvailable,
-        'pType': _productTypes.toString().split('.').last,
-
+        'pType': pTypesValue,
       });
       //Navigator.canPop(context) ? Navigator.pop(context) : null;
       imageFile = null;
-    }
-    catch(e){
+    } catch (e) {
       Fluttertoast.showToast(msg: e.toString());
     }
   }
+
   @override
   Widget build(BuildContext context) {
     final _productfilldata = GlobalKey<FormState>();
@@ -168,49 +159,46 @@ class _uploadProduct extends State<uploadProduct> {
         children: <Widget>[
           //add images container
           Container(
-
             padding: EdgeInsets.all(20.0),
             margin: EdgeInsets.all(15.0),
             height: 180.0,
             width: double.infinity,
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.lightBlueAccent,
-          ),
-
-          child: Column(
-           children: <Widget>[
-             CircleAvatar(
-               radius: 40,
-               child: imageFile != null ? ClipRRect(
-                 borderRadius: BorderRadius.circular(50),
-                 child: Image.file(
-                   imageFile!,
-                   width: 100,
-                   height: 100,
-                   fit: BoxFit.fitHeight,
-                 ),
-               )
-                   : Container(
-                 decoration: BoxDecoration(
-                     color: Colors.grey[200],
-                     borderRadius: BorderRadius.circular(50)),
-                 width: 100,
-                 height: 100,
-                 child: Icon(
-                   Icons.camera_alt,
-                   color: Colors.grey[800],
-                 ),
-               )
-             ),
-             ElevatedButton(
-                 onPressed: (){
-                   _showImageDialog();
-                 },
-                 child: Text("Add images")),
-           ],
-
-          ),
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.lightBlueAccent,
+            ),
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                    radius: 40,
+                    child: imageFile != null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.file(
+                              imageFile!,
+                              width: 100,
+                              height: 100,
+                              fit: BoxFit.fitHeight,
+                            ),
+                          )
+                        : Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(50)),
+                            width: 100,
+                            height: 100,
+                            child: Icon(
+                              Icons.camera_alt,
+                              color: Colors.grey[800],
+                            ),
+                          )),
+                ElevatedButton(
+                    onPressed: () {
+                      _showImageDialog();
+                    },
+                    child: Text("Add images")),
+              ],
+            ),
           ),
           Container(
             child: Center(
@@ -218,65 +206,108 @@ class _uploadProduct extends State<uploadProduct> {
             ),
           ),
 
-          //fill in form container
+          //Form Container that contains information about the product
           Container(
-            padding: EdgeInsets.all(10.0),
+            padding: EdgeInsets.all(15.0),
             margin: EdgeInsets.all(10.0),
-          child: Form(
-            key: _productfilldata,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  controller: productName,
-                  decoration: const InputDecoration(
-                    hintText: 'Enter the product name',
+            child: Form(
+              key: _productfilldata,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: productName,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter the product name',
+                    ),
                   ),
-                ),
-                TextFormField(
-                  controller: price,
-                  decoration: const InputDecoration(
-                    hintText: 'Price',
+                  TextFormField(
+                    controller: price,
+                    decoration: const InputDecoration(
+                      hintText: 'Price',
+                    ),
                   ),
-                ),
-
-                    RadioListTile<pTypes>(
-                      title: const Text('Laptops'),
-                      value: pTypes.laptops,
-                      groupValue: _productTypes,
-                      onChanged: (pTypes? value){
+                  Row(children: <Widget>[
+                    Flexible(
+                        flex: 2,
+                        child: Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: Text('Condition'))),
+                    Flexible(
+                        child: DropdownButton<String>(
+                      value: conditionValue,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.blue),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.blue,
+                      ),
+                      onChanged: (String? newValue) {
                         setState(() {
-                          _productTypes = value;
+                          conditionValue = newValue!;
                         });
                       },
-                    ),
-                    RadioListTile<pTypes>(
-                      title: const Text('Desktops'),
-                      value: pTypes.desktops,
-                      groupValue: _productTypes,
-                      onChanged: (pTypes? value){
+                      items: <String>['Excellent', 'Moderate', 'Poor']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ))
+                  ]),
+                  Row(children: <Widget>[
+                    Flexible(
+                        flex: 2,
+                        child: Padding(
+                            padding: const EdgeInsets.only(right: 10.0),
+                            child: Text('Type of device'))),
+                    Flexible(
+                        child: DropdownButton<String>(
+                      value: pTypesValue,
+                      icon: const Icon(Icons.arrow_drop_down),
+                      iconSize: 24,
+                      elevation: 16,
+                      style: const TextStyle(color: Colors.blue),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.blue,
+                      ),
+                      onChanged: (String? newValue) {
                         setState(() {
-                          _productTypes = value;
+                          pTypesValue = newValue!;
                         });
                       },
+                      items: <String>['Phone', 'Laptop', 'Desktop', 'Charger']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ))
+                  ]),
+                  TextFormField(
+                    controller: zipCode,
+                    decoration: const InputDecoration(
+                      hintText: 'Zip Code',
                     ),
-
-
-                ElevatedButton(
-                    onPressed: ()async{
-                      if(imageFile != null){
-                        _upload_images();
-                      }
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HomeScreen()));
-
-                    },
-                    child: Text("Make Available to Rent!"))
-              ],
+                  ),
+                  ElevatedButton(
+                      onPressed: () async {
+                        if (imageFile != null) {
+                          _upload_images();
+                        }
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()));
+                      },
+                      child: Text("Make Available to Rent!"))
+                ],
+              ),
             ),
-
-          ),
           )
         ],
       ),
