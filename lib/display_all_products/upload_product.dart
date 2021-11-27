287f60fab12c2a43e75b87616f215afdf8780219
Leonardo Ruiz <lruiz0333@gmail.com>
2021-11-26 22:37:48 -0600

    make upload_product scrollable

--- lib/display_all_products/upload_product.dart
+++ lib/display_all_products/upload_product.dart
@@ -158,165 +158,166 @@ class _uploadProduct extends State<uploadProduct> {
   Widget build(BuildContext context) {
     final _productfilldata = GlobalKey<FormState>();
     return Scaffold(
-      backgroundColor: Colors.blue[50],
-      body: Column(
-        children: <Widget>[
-          //add images container
-          Container(
-            padding: EdgeInsets.all(20.0),
-            margin: EdgeInsets.all(15.0),
-            height: 180.0,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.lightBlueAccent,
-            ),
-            child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                    radius: 40,
-                    child: imageFile != null
-                        ? ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Image.file(
-                              imageFile!,
-                              width: 100,
-                              height: 100,
-                              fit: BoxFit.fitHeight,
-                            ),
-                          )
-                        : Container(
-                            decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                borderRadius: BorderRadius.circular(50)),
-                            width: 100,
-                            height: 100,
-                            child: Icon(
-                              Icons.camera_alt,
-                              color: Colors.grey[800],
-                            ),
-                          )),
-                ElevatedButton(
-                    onPressed: () {
-                      _showImageDialog();
-                    },
-                    child: Text("Add images")),
-              ],
-            ),
-          ),
-          Container(
-            child: Center(
-              child: Text("Product Details"),
-            ),
-          ),
-
-          //Form Container that contains information about the product
-          Container(
-            padding: EdgeInsets.all(15.0),
-            margin: EdgeInsets.all(10.0),
-            child: Form(
-              key: _productfilldata,
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    controller: productName,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter the product name',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: price,
-                    decoration: const InputDecoration(
-                      hintText: 'Price',
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  Row(children: <Widget>[
-                    const Flexible(
-                        flex: 2,
-                        child: Padding(
-                            padding: EdgeInsets.only(right: 50.0),
-                            child: Text('Condition'))),
-                    Flexible(
-                        child: DropdownButton<String>(
-                      value: conditionValue,
-                      icon: const Icon(Icons.arrow_drop_down),
-                      iconSize: 24,
-                      elevation: 16,
-                      style: const TextStyle(color: Colors.blue),
-                      underline: Container(
-                        height: 2,
-                        color: Colors.blue,
-                      ),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          conditionValue = newValue!;
-                        });
-                      },
-                      items: product_condition.map((value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                    ))
-                  ]),
-                  const SizedBox(height: 20.0),
-                  Row(children: <Widget>[
-                    const Flexible(
-                        flex: 2,
-                        child: Padding(
-                            padding: EdgeInsets.only(right: 30.0),
-                            child: Text('Type of device'))),
-                    Flexible(
-                        child: DropdownButton<String>(
-                      value: pTypesValue,
-                      icon: const Icon(Icons.arrow_drop_down),
-                      iconSize: 24,
-                      elevation: 16,
-                      style: const TextStyle(color: Colors.blue),
-                      underline: Container(
-                        height: 2,
-                        color: Colors.blue,
-                      ),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          pTypesValue = newValue!;
-                        });
-                      },
-                      items: product_types.map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                    ))
-                  ]),
-                  const SizedBox(height: 20.0),
-                  TextFormField(
-                    controller: zipCode,
-                    decoration: const InputDecoration(
-                      hintText: 'Zip Code',
-                    ),
-                  ),
-                  const SizedBox(height: 20.0),
-                  ElevatedButton(
-                      onPressed: () async {
-                        if (imageFile != null) {
-                          _upload_images();
-                        }
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => HomeScreen()));
-                      },
-                      child: Text("Make Available to Rent!"))
-                ],
+        backgroundColor: Colors.blue[50],
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              //add images container
+              Container(
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.all(15.0),
+                height: 180.0,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Colors.lightBlueAccent,
+                ),
+                child: Column(
+                  children: <Widget>[
+                    CircleAvatar(
+                        radius: 40,
+                        child: imageFile != null
+                            ? ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: Image.file(
+                                  imageFile!,
+                                  width: 100,
+                                  height: 100,
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              )
+                            : Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.grey[200],
+                                    borderRadius: BorderRadius.circular(50)),
+                                width: 100,
+                                height: 100,
+                                child: Icon(
+                                  Icons.camera_alt,
+                                  color: Colors.grey[800],
+                                ),
+                              )),
+                    ElevatedButton(
+                        onPressed: () {
+                          _showImageDialog();
+                        },
+                        child: Text("Add images")),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-    );
+              Container(
+                child: Center(
+                  child: Text("Product Details"),
+                ),
+              ),
+
+              //Form Container that contains information about the product
+              Container(
+                padding: EdgeInsets.all(15.0),
+                margin: EdgeInsets.all(10.0),
+                child: Form(
+                  key: _productfilldata,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        controller: productName,
+                        decoration: const InputDecoration(
+                          hintText: 'Enter the product name',
+                        ),
+                      ),
+                      TextFormField(
+                        controller: price,
+                        decoration: const InputDecoration(
+                          hintText: 'Price',
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      Row(children: <Widget>[
+                        const Flexible(
+                            flex: 2,
+                            child: Padding(
+                                padding: EdgeInsets.only(right: 50.0),
+                                child: Text('Condition'))),
+                        Flexible(
+                            child: DropdownButton<String>(
+                          value: conditionValue,
+                          icon: const Icon(Icons.arrow_drop_down),
+                          iconSize: 24,
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.blue),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.blue,
+                          ),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              conditionValue = newValue!;
+                            });
+                          },
+                          items: product_condition.map((value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ))
+                      ]),
+                      const SizedBox(height: 20.0),
+                      Row(children: <Widget>[
+                        const Flexible(
+                            flex: 2,
+                            child: Padding(
+                                padding: EdgeInsets.only(right: 30.0),
+                                child: Text('Type of device'))),
+                        Flexible(
+                            child: DropdownButton<String>(
+                          value: pTypesValue,
+                          icon: const Icon(Icons.arrow_drop_down),
+                          iconSize: 24,
+                          elevation: 16,
+                          style: const TextStyle(color: Colors.blue),
+                          underline: Container(
+                            height: 2,
+                            color: Colors.blue,
+                          ),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              pTypesValue = newValue!;
+                            });
+                          },
+                          items: product_types.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ))
+                      ]),
+                      const SizedBox(height: 20.0),
+                      TextFormField(
+                        controller: zipCode,
+                        decoration: const InputDecoration(
+                          hintText: 'Zip Code',
+                        ),
+                      ),
+                      const SizedBox(height: 20.0),
+                      ElevatedButton(
+                          onPressed: () async {
+                            if (imageFile != null) {
+                              _upload_images();
+                            }
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomeScreen()));
+                          },
+                          child: Text("Make Available to Rent!"))
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
